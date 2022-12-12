2a4b9580cd41e773112475301a486d920a2147b5
Muhammad Fahreza Azka Arafat <beyond2lte@gmail.com>
2022-12-12 09:56:24 +0700

    Correcting URL Typo.

--- investops/lib/data/sendDeleteFeedback.dart
+++ investops/lib/data/sendDeleteFeedback.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 
 Future<bool> sendDeleteFeedback(id) async {
   var url = Uri.parse('https://investops.up.railway.app/suggestionbox/deleteFeedbackFlutter/');
-  // var url = Uri.parse('http://localhost:8000/suggestionbox/deleteFeebdackFlutter/');
+  // var url = Uri.parse('http://localhost:8000/suggestionbox/deleteFeedbackFlutter/');
   Map<String, String> requestBody = {
     'id': id,
   };
