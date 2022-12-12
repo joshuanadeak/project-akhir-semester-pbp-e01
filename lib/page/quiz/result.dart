9ec3265073561e2a7a47fc14fa2f00e8e638283f
joanisva <joan.isva@ui.ac.id>
2022-12-12 21:37:44 +0700

    Merapihkan tampilan quiz

--- lib/page/quiz/result.dart
+++ lib/page/quiz/result.dart
@@ -21,47 +21,45 @@ class _ResultPageState extends State<ResultPage> {
       ),
       drawer: const UniversalDrawer(),
       body: Center(
-        child:
-          Column(
-            children: [
-              const Spacer(flex: 2),
-              Text(
-                "Thank you for taking the quiz, $nama", //tambahin nama
-                style:const TextStyle(color: Colors.white,
-                      fontFamily: 'Alexandria',
-                      fontSize: 18),
-              ),
-              const Spacer(),
-              const Text(
-                "Result", //tambahin nama
-                style:TextStyle(color: Colors.white,
-                      fontFamily: 'Alexandria',
-                      fontSize: 24),
-              ),
-              Text(
-                "$nilaiKuis", //tambahin score
-                style:const TextStyle(color: Colors.white,
-                      fontFamily: 'Alexandria',
-                      fontSize: 32),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 74, 229, 154),
-                  padding: const EdgeInsets.all(16.0),
-                  textStyle: const TextStyle(fontSize: 20, color: Colors.white),
-                ),
-                child: const Text('Home'),
-                onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const MyMainPage()),
-                  );
-                }, // on pressed leads to quiz.
-              ),
-            ],
-          )
-      ),
+          child: Column(
+        children: [
+          const Spacer(flex: 1),
+          Text(
+            "Thank you for taking the quiz, $nama", //tambahin nama
+            style: const TextStyle(
+                color: Colors.white, fontFamily: 'Alexandria', fontSize: 18),
+          ),
+          const Spacer(flex: 1),
+          const Text(
+            "Result", //tambahin nama
+            style: TextStyle(
+                color: Colors.white, fontFamily: 'Alexandria', fontSize: 24),
+          ),
+          Text(
+            "$nilaiKuis/100", //tambahin score
+            style: const TextStyle(
+                color: Color.fromARGB(255, 74, 229, 154),
+                fontFamily: 'Alexandria',
+                fontSize: 32),
+          ),
+          const Spacer(flex: 1),
+          TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 74, 229, 154),
+              padding: const EdgeInsets.all(16.0),
+              textStyle: const TextStyle(fontSize: 20, color: Colors.black),
+            ),
+            child: const Text('Home'),
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const MyMainPage()),
+              );
+            }, // on pressed leads to quiz.
+          ),
+          const Spacer(flex: 1)
+        ],
+      )),
     );
   }
 }
